38024d3029accae56393a4284eef0298e0298133
Pratush Bose <bosepratush@gmail.com>
2025-02-20 10:16:28 +0530

    adding top bar to main div

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,19 @@
-import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar"
-import { AppSidebar } from "./app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { AppSidebar } from "./app-sidebar";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
+      <main className="m-2 w-full">
+        <div className="flex items-center gap-2 rounded-md border border-sidebar-border bg-sidebar p-2 px-4 shadow">
+          <div className="ml-auto"></div>
+          <UserButton />
+        </div>
         {/* <SidebarTrigger /> */}
         {children}
       </main>
     </SidebarProvider>
-  )
+  );
 }
