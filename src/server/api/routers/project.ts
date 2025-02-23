940201ea5a4f2400137ed25424b452f8236313ce
Pratush Bose <bosepratush@gmail.com>
2025-02-23 22:26:21 +0530

    created createProjects and getProjects method in projects router

---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,40 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
+
+export const projectRouter = createTRPCRouter({
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        githubUrl: z.string(),
+        githubToken: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      console.log("userID: ", ctx.user.userId);
+      const project = await ctx.db.project.create({
+        data: {
+          name: input.name,
+          githubUrl: input.githubUrl,
+          userToProjects: {
+            create: {
+              userId: ctx.user.userId!,
+            },
+          },
+        },
+      });
+      return project;
+    }),
+  getProjects: protectedProcedure.query(async ({ ctx }) => {
+    const projects = await ctx.db.project.findMany({
+      where: {
+        userToProjects: {
+          some: {
+            userId: ctx.user.userId!,
+          },
+        },
+      },
+    });
+    return projects;
+  }),
+});
