05bf776ed56ef69fb26e9501d347ecad89917c45
Pratush Bose <bosepratush@gmail.com>
2025-02-23 22:24:41 +0530

    removed unused import of post.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,5 @@ import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
-  return (
-    <div>hello world</div>
-  );
+  return <div>hello world</div>;
 }
