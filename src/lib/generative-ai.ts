ed934c4d2e9b4f1509f6bd0e25bfc83aeb7d6f1c
Pratush Bose <bosepratush@gmail.com>
2025-02-27 12:57:49 +0530

    started work on generative ai from vertex

---
+++ src/lib/generative-ai.ts
@@ -0,0 +1,5 @@
+import {getVertexAI, getGenerativeModel} from 'firebase/vertexai'
+import { firebaseApp } from './firebase';
+
+const vertexAI = getVertexAI(firebaseApp)
+export const getCommitSummary = async () => {};
