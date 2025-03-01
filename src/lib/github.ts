ac39be7237a8d5a3f0cdc2839d15c23a9aa68636
Pratush Bose <bosepratush@gmail.com>
2025-03-01 21:17:15 +0530

    added some utlity functions in github.ts

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,6 +1,8 @@
 import { Octokit } from "octokit";
 import axios from "axios";
 import { getCommitSummary } from "./generative-ai";
+import { retry } from "./utils";
+import { db } from "~/server/db";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
 
@@ -12,7 +14,7 @@ type Response = {
   commitDate: string;
 };
 
-export const pollCommits = async (githubUrl: string) => {
+export const processCommits = async (githubUrl: string) => {
   const [owner, repo] = githubUrl.split("/").slice(-2);
   if (!owner || !repo) {
     throw new Error("Invalid GitHub URL");
@@ -44,6 +46,69 @@ export const pollCommits = async (githubUrl: string) => {
         },
       },
     );
-    await getCommitSummary(data);
+    await retry(() => getCommitSummary(data), {
+      maxAttempts: 5,
+      initialDelay: 60000,
+      maxDelay: 70000,
+      backoffFactor: 3,
+    });
+    // await getCommitSummary(data);
+  });
+};
+
+export const pollCommits = async (projectId: string) => {
+  const projectInDb = await getProjectFromDb(projectId);
+  if (projectInDb === null) {
+    throw new Error("Project not found");
+  }
+  const topTenCommits = await latestCommits(projectInDb.githubUrl);
+  const commitsInDb = await getCommitsForProjectInDb(projectId);
+
+};
+
+const unProcessedCommits = async (a: any[], b: any[]) => {
+  return a.filter((commit) => !b.find((c) => c.commitHash === commit.sha));
+}
+
+const getProjectFromDb = async (projectId: string) => {
+  const project = await db.project.findUnique({
+    where: {
+      id: projectId,
+    },
+  });
+  return project;
+};
+
+const latestCommits = async (githubUrl: string) => {
+  const commitsInGithub = await getCommitsFromGithub(githubUrl);
+  const sortedCommits = sortCommitsByTimestamp(commitsInGithub);
+  return sortedCommits.slice(0, 10);
+};
+
+const getCommitsFromGithub = async (githubUrl: string) => {
+  const [owner, repo] = githubUrl.split("/").slice(-2);
+  if (!owner || !repo) {
+    throw new Error("Invalid GitHub URL");
+  }
+  const { data } = await octokit.rest.repos.listCommits({
+    owner,
+    repo,
+  });
+  return data;
+};
+
+const sortCommitsByTimestamp = (commits: any[]) => {
+  return commits.sort(
+    (a: any, b: any) =>
+      new Date(b.commit.author.date).getTime() -
+      new Date(a.commit.author.date).getTime(),
+  );
+};
+
+const getCommitsForProjectInDb = async (projectId: string) => {
+  const commits = await db.commit.findMany({
+    where: {
+      projectId,
+    },
   });
 };
