199b733f9f75ff7009e0a046965fc86162b8f9d3
Pratush Bose <bosepratush@gmail.com>
2025-03-03 12:46:20 +0530

    playing more with typescript and creating the pipeline to process commit messages at the time of project creation

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { getCommitSummary } from "./generative-ai";
 import { retry } from "./utils";
 import { db } from "~/server/db";
+import { Commit } from "@prisma/client";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
 
@@ -14,48 +15,6 @@ type Response = {
   commitDate: string;
 };
 
-export const processCommits = async (githubUrl: string) => {
-  const [owner, repo] = githubUrl.split("/").slice(-2);
-  if (!owner || !repo) {
-    throw new Error("Invalid GitHub URL");
-  }
-  const { data } = await octokit.rest.repos.listCommits({
-    owner,
-    repo,
-  });
-
-  const sortedCommits = data.sort(
-    (a: any, b: any) =>
-      new Date(b.commit.author.date).getTime() -
-      new Date(a.commit.author.date).getTime(),
-  );
-  const topTenLatestCommits = sortedCommits.slice(0, 10).map((commit: any) => ({
-    commitHash: commit.sha as string,
-    commitMessage: commit.commit.message,
-    commitAuthorName: commit.commit?.author?.name ?? "",
-    commitAuthorAvatar: commit.author?.avatar_url ?? "",
-    commitDate: commit.commit?.author?.date ?? "",
-  }));
-
-  topTenLatestCommits.forEach(async (commit) => {
-    const { data } = await axios.get(
-      `${githubUrl}/commit/${commit.commitHash}.diff`,
-      {
-        headers: {
-          Accept: "application/vnd.github.v3.diff",
-        },
-      },
-    );
-    await retry(() => getCommitSummary(data), {
-      maxAttempts: 5,
-      initialDelay: 60000,
-      maxDelay: 70000,
-      backoffFactor: 3,
-    });
-    // await getCommitSummary(data);
-  });
-};
-
 export const pollCommits = async (projectId: string) => {
   const projectInDb = await getProjectFromDb(projectId);
   if (projectInDb === null) {
@@ -63,12 +22,59 @@ export const pollCommits = async (projectId: string) => {
   }
   const topTenCommits = await latestCommits(projectInDb.githubUrl);
   const commitsInDb = await getCommitsForProjectInDb(projectId);
+  const unprocessedCommits: Response[] = await unProcessedCommits(
+    commitsInDb,
+    topTenCommits,
+  );
 
+  const summaryResponse = await Promise.allSettled(
+    unprocessedCommits.map((commit) => {
+      return summariseCommit(projectInDb.githubUrl, commit.commitHash);
+    }),
+  );
+
+  const summaries = summaryResponse.map((response) => {
+    console.log("response", response);
+    if (response.status === "fulfilled") {
+      return response.value as string;
+    }
+    return "";
+  });
+  const commits = await db.commit.createMany({
+    data: summaries.map((summary, index) => {
+      console.log(`processing commit ${index}`);
+      return {
+        projectId,
+        commitHash: unprocessedCommits[index]!.commitHash,
+        commitMessage: unprocessedCommits[index]!.commitMessage,
+        commitAuthorName: unprocessedCommits[index]!.commitAuthorName,
+        commitAuthorAvatar: unprocessedCommits[index]!.commitAuthorAvatar,
+        commitDate: unprocessedCommits[index]!.commitDate,
+        summary,
+      };
+    }),
+  });
+  return commits;
 };
 
-const unProcessedCommits = async (a: any[], b: any[]) => {
-  return a.filter((commit) => !b.find((c) => c.commitHash === commit.sha));
-}
+const summariseCommit = async (githubUrl: string, commitHash: string) => {
+  const contents = await getCommitContents(githubUrl, commitHash);
+  return await retry(() => getCommitSummary(contents), {
+    maxAttempts: 5,
+    initialDelay: 60000,
+    maxDelay: 70000,
+    backoffFactor: 3,
+  });
+};
+
+const unProcessedCommits = async (
+  a: Commit[],
+  b: Response[],
+): Promise<Response[]> => {
+  return b.filter(
+    (commit) => !a.some((c) => c.commitHash === commit.commitHash),
+  );
+};
 
 const getProjectFromDb = async (projectId: string) => {
   const project = await db.project.findUnique({
@@ -79,13 +85,13 @@ const getProjectFromDb = async (projectId: string) => {
   return project;
 };
 
-const latestCommits = async (githubUrl: string) => {
-  const commitsInGithub = await getCommitsFromGithub(githubUrl);
+const latestCommits = async (githubUrl: string): Promise<Response[]> => {
+  const commitsInGithub: Response[] = await getCommitsFromGithub(githubUrl);
   const sortedCommits = sortCommitsByTimestamp(commitsInGithub);
   return sortedCommits.slice(0, 10);
 };
 
-const getCommitsFromGithub = async (githubUrl: string) => {
+const getCommitsFromGithub = async (githubUrl: string): Promise<Response[]> => {
   const [owner, repo] = githubUrl.split("/").slice(-2);
   if (!owner || !repo) {
     throw new Error("Invalid GitHub URL");
@@ -94,21 +100,43 @@ const getCommitsFromGithub = async (githubUrl: string) => {
     owner,
     repo,
   });
-  return data;
+
+  const responses = data.map((commit: any) => ({
+    commitHash: commit.sha as string,
+    commitMessage: commit.commit.message,
+    commitAuthorName: commit.commit?.author?.name ?? "",
+    commitAuthorAvatar: commit.author?.avatar_url ?? "",
+    commitDate: commit.commit?.author?.date ?? "",
+  }));
+  return responses;
 };
 
-const sortCommitsByTimestamp = (commits: any[]) => {
+const sortCommitsByTimestamp = (commits: Response[]): Response[] => {
   return commits.sort(
-    (a: any, b: any) =>
-      new Date(b.commit.author.date).getTime() -
-      new Date(a.commit.author.date).getTime(),
+    (a: Response, b: Response) =>
+      new Date(b.commitDate).getTime() - new Date(a.commitDate).getTime(),
   );
 };
 
-const getCommitsForProjectInDb = async (projectId: string) => {
+const getCommitsForProjectInDb = async (
+  projectId: string,
+): Promise<Commit[]> => {
   const commits = await db.commit.findMany({
     where: {
       projectId,
     },
   });
+  return commits;
+};
+
+const getCommitContents = async (
+  githubUrl: string,
+  commitHash: string,
+): Promise<string> => {
+  const { data } = await axios.get(`${githubUrl}/commit/${commitHash}.diff`, {
+    headers: {
+      Accept: "application/vnd.github.v3.diff",
+    },
+  });
+  return data;
 };
