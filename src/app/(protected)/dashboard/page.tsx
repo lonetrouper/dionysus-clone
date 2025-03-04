a993bd9173bf54795670358933c2299dcc5187d4
Pratush Bose <bosepratush@gmail.com>
2025-03-04 11:40:20 +0530

    created the layout for dashboard page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,45 @@
 "use client";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import useProject from "~/hooks/use-project";
+import CommitLog from "./commit-log";
 
 const Dashboard = () => {
   const { project } = useProject();
   if (project === undefined) return null;
-  // const polledCommits = pollCommits(project?.githubUrl);
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <p className="text-sm font-medium text-white">
+              This project is linked to{" "}
+              <Link
+                href={project.githubUrl}
+                className="inline-flex items-center text-white/80 hover:underline"
+              >
+                {project.githubUrl}
+                <ExternalLink className="ml-1 size-4" />
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">
+          TeamMembers InviteButton ArchiveButton
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          <div>AskQuestionCard</div>
+          <div>MeetingCard</div>
+        </div>
+      </div>
+      <div className="mt-8"></div>
+      <CommitLog /> 
+    </div>
+  );
 };
 
 export default Dashboard;
