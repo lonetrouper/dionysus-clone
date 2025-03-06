796a1e71fbd6500757ebffa5ef00394e8ddaef67
Pratush Bose <bosepratush@gmail.com>
2025-03-06 09:26:00 +0530

    some more testing on langchain pdf loader

--- src/server/api/routers/langchain.ts
+++ src/server/api/routers/langchain.ts
@@ -1,8 +1,10 @@
 import { langchainPractice } from "~/lib/langchain-practice";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { pdfLoader } from "~/lib/pdf-loader";
 
 export const langchainRouter = createTRPCRouter({
-    getLangchainResponse: protectedProcedure.query(async({ctx}) => {
-        langchainPractice();
-    })
-})
+  getLangchainResponse: protectedProcedure.query(async ({ ctx }) => {
+    // langchainPractice();
+    pdfLoader();
+  }),
+});
