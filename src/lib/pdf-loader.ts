4b96736111769d6e1c601dfe12edd91c331484b3
Pratush Bose <bosepratush@gmail.com>
2025-03-07 11:50:14 +0530

    rag implementation in langchain

--- src/lib/pdf-loader.ts
+++ src/lib/pdf-loader.ts
@@ -1,5 +1,13 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { ChatVertexAI, VertexAIEmbeddings } from "@langchain/google-vertexai";
+import {
+  DistanceStrategy,
+  PGVectorStore,
+} from "@langchain/community/vectorstores/pgvector";
+import { Client, PoolConfig } from "pg";
+import { pull } from "langchain/hub";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 export const pdfLoader = async () => {
   const loader = new PDFLoader(
@@ -9,7 +17,7 @@ export const pdfLoader = async () => {
   const docs = await loader.load();
   if (docs[0] && docs[0].pageContent) {
     // console.log(docs[0].metadata)
-    console.log(docs[0].pageContent);
+    // console.log(docs[0].pageContent);
   } else {
     console.log("Document content is undefined");
   }
@@ -20,5 +28,64 @@ export const pdfLoader = async () => {
 
   const allSplits = await textSplitter.splitDocuments(docs);
 
-  allSplits.length;
+  const embeddingModel = new VertexAIEmbeddings({
+    model: "text-embedding-004",
+  });
+
+  const embeddings = await Promise.all(
+    allSplits.map((split) => {
+      return embeddingModel.embedQuery(split.pageContent);
+    }),
+  );
+
+  embeddings.forEach((embedding) => {
+    console.log("embedding", embedding.slice(0, 5));
+  });
+
+  // const connectionString = process.env.DATABASE_URL;
+  // const pgClient = new Client({connectionString})
+  // await pgClient.connect()
+
+  const config = {
+    postgresConnectionOptions: {
+      type: "postgres",
+      host: "aws-0-ap-south-1.pooler.supabase.com",
+      port: 5432,
+      user: "postgres.iywsjeddkphvnaitzrts",
+      password: "j5n-2Ti_YSEmKxB",
+      database: "postgres",
+    } as PoolConfig,
+    tableName: "testlangchainjs",
+    columns: {
+      idColumnName: "id",
+      vectorColumnName: "vector",
+      contentColumnName: "content",
+      metadataColumnName: "metadata",
+    },
+    // supported distance strategies: cosine (default), innerProduct, or euclidean
+    distanceStrategy: "cosine" as DistanceStrategy,
+  };
+
+  const vectorStore = await PGVectorStore.initialize(embeddingModel, config);
+  await vectorStore.addDocuments(allSplits);
+
+  const llm = new ChatVertexAI({
+    model: "gemini-1.5-flash",
+    temperature: 0,
+  });
+
+  const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+  const question = "Which product supports DHCP";
+
+  const retrievedDocuments = await vectorStore.similaritySearch(question);
+  const docContent = retrievedDocuments
+    .map((doc) => doc.pageContent)
+    .join("\n");
+  const messages = await promptTemplate.invoke({
+    question: question,
+    context: docContent,
+  });
+  const answer = await llm.invoke(messages);
+  console.log("RAG answer", answer);
 };
