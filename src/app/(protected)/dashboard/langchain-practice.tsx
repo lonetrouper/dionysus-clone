ea4d5de11b5a4629146932047c0a9e67d6e40d1d
Pratush Bose <bosepratush@gmail.com>
2025-03-12 12:35:30 +0530

    changing " to '

--- src/app/(protected)/dashboard/langchain-practice.tsx
+++ src/app/(protected)/dashboard/langchain-practice.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { api } from '~/trpc/react'
+import React from "react";
+import { api } from "~/trpc/react";
 
 const LangchainPractice = () => {
-    const langchain = api.langchain.getLangchainResponse.useQuery()
-  return (
-    <div>LangchainPractice</div>
-  )
-}
+  const langchain = api.langchain.getLangchainResponse.useQuery();
+  return <div>LangchainPractice</div>;
+};
 
-export default LangchainPractice
+export default LangchainPractice;
