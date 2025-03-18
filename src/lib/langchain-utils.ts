19397cc716719b499a106134756d650573875602
Pratush Bose <bosepratush@gmail.com>
2025-03-18 11:50:56 +0530

    really happy about this piece of code. I used addModels to add embeddings to a table created by prisma

--- src/lib/langchain-utils.ts
+++ src/lib/langchain-utils.ts
@@ -1,7 +1,9 @@
-import { ChatVertexAI } from "@langchain/google-vertexai";
+import { ChatVertexAI, VertexAIEmbeddings } from "@langchain/google-vertexai";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { PromptTemplate } from "@langchain/core/prompts";
-import { Document } from "@langchain/core/documents";
+import { PrismaVectorStore } from "@langchain/community/vectorstores/prisma";
+import { Prisma, SourceCodeEmbeddings } from "@prisma/client";
+import { db } from "~/server/db";
 
 const model = new ChatVertexAI({
   model: "gemini-1.5-flash",
@@ -18,8 +20,8 @@ export const langchainPractice = async () => {
   console.log("langchain test response", response);
 };
 
-export const getCodeSummary = async (doc: Document) => {
-  const code = doc.pageContent.slice(0, 10000);
+export const getCodeSummary = async (fileName: string, code: string) => {
+  // const code = doc.pageContent.slice(0, 10000);
   const codeSummaryPromptTemplate =
     PromptTemplate.fromTemplate(`You are an intelligent senior software engineer who specializes in onboarding junior software engineers onto projects.
         You are onboarding a junior software engineer who is new to the project and you are trying to explain the purpose of {fileName} file.
@@ -29,9 +31,30 @@ export const getCodeSummary = async (doc: Document) => {
         ---
         Please give a summary of no more than 100 words of the code above`);
   const codeSummaryMessage = await codeSummaryPromptTemplate.invoke({
-    fileName: doc.metadata.source,
+    fileName,
+    // fileName: doc.metadata.source,
     code,
   });
   const response = await model.invoke(codeSummaryMessage);
-  return response.content;
+  return response.content.toString();
+};
+
+export const addSourceCodeEmbeddings = async (sourceCode: SourceCodeEmbeddings) => {
+  const embeddingModel = new VertexAIEmbeddings({
+    model: "text-embedding-004",
+  });
+  const vectorStore = PrismaVectorStore.withModel<SourceCodeEmbeddings>(
+    db,
+  ).create(embeddingModel, {
+    prisma: Prisma,
+    tableName: "SourceCodeEmbeddings",
+    vectorColumnName: "summaryEmbeddings",
+    columns: {
+      id: PrismaVectorStore.IdColumn,
+      summary: PrismaVectorStore.ContentColumn,
+    },
+  });
+
+  await vectorStore.addModels([sourceCode]);
+
 };
