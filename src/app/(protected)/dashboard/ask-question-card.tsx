ec108dec55fa4ff4db4bd0eccdccbcec76b841e8
Pratush Bose <bosepratush@gmail.com>
2025-03-21 14:02:23 +0530

    add more ui to ask-question-card

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -13,37 +13,69 @@ import { Textarea } from "~/components/ui/textarea";
 import useProject from "~/hooks/use-project";
 import useRefetch from "~/hooks/use-refetch";
 import { api } from "~/trpc/react";
+import { askQuestionServerComponent } from "./actions";
+import { toast } from "sonner";
+import CodeReferences from "./code-references";
 
 const AskQuestionCard = () => {
   const [question, setQuestion] = React.useState<string>("");
   const [open, setOpen] = React.useState<boolean>(false);
   const [answer, setAnswer] = React.useState<string>("");
   const [loading, setLoading] = React.useState<boolean>(false);
+  const [fileReferences, setFileReferences] = React.useState<
+    {
+      fileName: string;
+      sourceCode: string;
+      summary: string;
+    }[]
+  >([]);
   const { projectId } = useProject();
-  const getAnswerMutation = api.project.getAnswer.useMutation()
+
+  const saveAnswer = api.project.saveAnswer.useMutation({
+    onSuccess: () => {
+      toast.success("Answer saved successfully");
+      refetch();
+    },
+    onError: (error) => {
+      toast.error("Failed to save answer");
+      console.log(error);
+    },
+  });
+  // const getAnswerMutation = api.project.getAnswer.useMutation()
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    setAnswer("")
+    setAnswer("");
+    // setFileReferences([]);
     e.preventDefault();
 
-    setLoading(true)
-    const { stream } = await getAnswerMutation.mutateAsync({question, projectId}); 
-    // const {stream } = await askQuestion(question, projectId);
+    setLoading(true);
+    const { stream, filesReferenced } = await askQuestionServerComponent(
+      question,
+      projectId,
+    );
+    filesReferenced.map((file) => {
+      const currentReference = {
+        fileName: file.fileName,
+        sourceCode: file.sourceCode,
+        summary: file.summary,
+      };
+      setFileReferences((prev) => [...prev, currentReference]);
+    });
 
     setOpen(true);
     const reader = stream.getReader();
-    while(true){
+    while (true) {
       const { done, value } = await reader.read();
-      if(done) break;
-      setAnswer(prev => prev + value);
+      if (done) break;
+      setAnswer((prev) => prev + value);
     }
-    setLoading(false)
+    setLoading(false);
   };
   const refetch = useRefetch();
   return (
     <>
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
+        <DialogContent className="sm:max-w-[80vw]" data-color-mode="light">
           <DialogHeader>
             <div className="flex items-center gap-2">
               <DialogTitle>
@@ -54,12 +86,28 @@ const AskQuestionCard = () => {
                   height={40}
                 />
               </DialogTitle>
-              <Button variant={"outline"}>Save Answer</Button>
+              <Button
+                variant={"outline"}
+                disabled={saveAnswer.isPending}
+                onClick={() => {
+                  saveAnswer.mutate({
+                    projectId,
+                    question,
+                    answer,
+                    fileReferences
+                  });
+                }}
+              >
+                Save Answer
+              </Button>
             </div>
           </DialogHeader>
-          <MDEditor.Markdown source={answer}/>
+          <MDEditor.Markdown source={answer} className="p-2 rounded-md"  />
           <div className="h-4"></div>
-          <Button type='submit' onClick={() => setOpen(false)}>Close</Button>
+          <CodeReferences fileReferences={fileReferences} />
+          <Button type="submit" onClick={() => setOpen(false)}>
+            Close
+          </Button>
         </DialogContent>
       </Dialog>
       <Card className="relative col-span-3">
@@ -74,7 +122,9 @@ const AskQuestionCard = () => {
               onChange={(e) => setQuestion(e.target.value)}
             />
             <div className="h-4"></div>
-            <Button type="submit" disabled={loading}>Ask ByteBlaze!</Button>
+            <Button type="submit" disabled={loading}>
+              Ask ByteBlaze!
+            </Button>
           </form>
         </CardContent>
       </Card>
