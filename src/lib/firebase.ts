1ddf038848c6472357085107c66a272a58af7836
Pratush Bose <bosepratush@gmail.com>
2025-03-25 11:49:26 +0530

    adding method to upload files to firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +16,49 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID 
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
+export const storage = getStorage(firebaseApp);
+
+export const uploadFile = async (
+  file: File,
+  setProgress?: (progress: number) => void,
+) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+          );
+          if (setProgress) setProgress(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.error(error);
+          reject(error);
+        },
+        async () => {
+          const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+          resolve(downloadUrl as string);
+        },
+      );
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
+};
