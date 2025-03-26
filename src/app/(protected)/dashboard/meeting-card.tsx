3d7e6f49660698be144a300825a7e28ee5990664
Pratush Bose <bosepratush@gmail.com>
2025-03-26 12:15:18 +0530

    handling file uploads in meeting card

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -6,12 +6,18 @@ import { useDropzone } from "react-dropzone";
 import useProject from "~/hooks/use-project";
 import { useRouter } from "next/navigation";
 import { uploadFile } from "~/lib/firebase";
+import { api } from "~/trpc/react";
+import { toast } from "sonner";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 const MeetingCard = () => {
   const [isUploading, setIsUploading] = React.useState(false);
   const { project } = useProject();
   const [progress, setProgress] = React.useState(0);
+
   const router = useRouter();
+
+  const uploadMeeting = api.project.uploadMeeting.useMutation();
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
@@ -28,28 +34,68 @@ const MeetingCard = () => {
       if (!file) {
         return;
       }
-      const downloadUrl = (await uploadFile(file as File, setProgress) as string)
+      const downloadUrl = (await uploadFile(
+        file as File,
+        setProgress,
+      )) as string;
+      uploadMeeting.mutate(
+        {
+          projectId: project.id,
+          meetingUrl: downloadUrl,
+          name: file.name,
+        },
+        {
+          onSuccess: () => {
+            toast.success("Meeting uploaded successfully");
+            router.push("/meetings");
+          },
+          onError: (error) => {
+            toast.error(error.message);
+          },
+        },
+      );
+      setIsUploading(false);
     },
   });
 
   return (
-    <Card className="col-span-2 flex flex-col items-center justify-center p-10">
-      <Presentation className="h-10 w-10 animate-bounce" />
-      <h3 className="mt-2 text-sm font-semibold text-gray-900">
-        Create a new meeting
-      </h3>
-      <p className="mt-1 text-center text-sm font-semibold text-gray-500">
-        Analyse your meeting with ByteBlaze
-        <br />
-        Powered by AI.
-      </p>
-      <div className="mt-6">
-        <Button disabled={isUploading}>
-          <Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-          Upload Meeting
-          <input className="hidden" />
-        </Button>
-      </div>
+    <Card
+      className="col-span-2 flex flex-col items-center justify-center p-10"
+      {...getRootProps()}
+    >
+      {!isUploading && (
+        <>
+          <Presentation className="h-10 w-10 animate-bounce" />
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a new meeting
+          </h3>
+          <p className="mt-1 text-center text-sm font-semibold text-gray-500">
+            Analyse your meeting with ByteBlaze
+            <br />
+            Powered by AI.
+          </p>
+          <div className="mt-6">
+            <Button disabled={isUploading}>
+              <Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()}/>
+            </Button>
+          </div>
+        </>
+      )}
+      {isUploading && (
+        <div className="">
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}%`}
+            className="size-20"
+            styles={buildStyles({
+              pathColor: "#2563eb",
+              textColor: "#2563eb",
+            })}
+          ></CircularProgressbar>
+        </div>
+      )}
     </Card>
   );
 };
