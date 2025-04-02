e8304ad9aa2d768c66fc1d4fa46d1ab3f07f3441
Pratush Bose <bosepratush@gmail.com>
2025-04-02 08:49:10 +0530

    archive-button component

--- src/app/(protected)/dashboard/archive-button.tsx
+++ src/app/(protected)/dashboard/archive-button.tsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
 import useProject from "~/hooks/use-project";
+import useRefetch from "~/hooks/use-refetch";
 import { api } from "~/trpc/react";
 
 const ArchiveButton = () => {
   const archiveProject = api.project.archiveProject.useMutation();
   const { projectId } = useProject();
+  const refetch = useRefetch();
   return (
     <Button
+      disabled={archiveProject.isPending}
+      size="sm"
+      variant="destructive"
       onClick={() => {
         const confirm = window.confirm(
           "Are you sure you want to archive this project?",
         );
-        if (confirm) archiveProject.mutate({ projectId });
+        if (confirm)
+          archiveProject.mutate(
+            { projectId },
+            {
+              onSuccess: () => {
+                toast.success("Project archived successfully");
+                refetch();
+              },
+              onError: (error) => {
+                console.log("error: ", error);
+                toast.error("Failed to archive project");
+              },
+            },
+          );
       }}
     >
       Archive
