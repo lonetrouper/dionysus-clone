0322b7a5d28217b953265277810ae3d50d05ee2d
Pratush Bose <bosepratush@gmail.com>
2025-04-02 14:24:19 +0530

    another trpc route to get credits of a user

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -170,4 +170,12 @@ export const projectRouter = createTRPCRouter({
         },
       });
     }),
+  getMyCredits: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.user.findUnique({
+      where: {
+        id: ctx.user.userId!,
+      },
+      select: { credits: true },
+    });
+  }),
 });
