71b88df3bdc3feb094fabbeff3e713d8604b01de
Pratush Bose <bosepratush@gmail.com>
2025-04-02 08:49:00 +0530

    invite button component

---
+++ src/app/(protected)/dashboard/invite-button.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { toast } from "sonner";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import useProject from "~/hooks/use-project";
+
+const InviteButon = () => {
+  const { projectId } = useProject();
+  const [open, setOpen] = React.useState(false);
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite Team Members</DialogTitle>
+          </DialogHeader>
+          <p className="text-sm text-gray-500">
+            Ask them to copy paste this link
+          </p>
+          <Input
+            className="mt-4"
+            readOnly
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${window.location.origin}/join/${projectId}`,
+              );
+              toast.success("copied to clipboard");
+            }}
+            value={`${window.location.origin}/join/${projectId}`}
+          ></Input>
+        </DialogContent>
+      </Dialog>
+      <Button size="sm" onClick={() => setOpen(true)}>
+        Invite Members
+      </Button>
+    </>
+  );
+};
+
+export default InviteButon;
