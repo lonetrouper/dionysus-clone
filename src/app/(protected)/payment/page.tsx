0b10a481b2dd23792ff02c1f65eac01d6e4ba762
Pratush Bose <bosepratush@gmail.com>
2025-04-03 11:59:41 +0530

    new component for handling razorpay payment gateway

---
+++ src/app/(protected)/payment/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
+import { useState } from "react";
+import Script from "next/script";
+
+function Payment() {
+  const [name, setName] = useState("Pratush");
+  const [email, setEmail] = useState("bosepratush@gmail.com");
+  const [amount, setAmount] = useState("0");
+  const [currency, setCurrency] = useState("INR");
+  const [loading, setLoading] = useState(false);
+  const createOrderId = async () => {
+    try {
+      const response = await fetch("/api/order", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          amount: parseFloat(amount) * 100,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      return data.orderId;
+    } catch (error) {
+      console.error("There was a problem with your fetch operation:", error);
+    }
+  };
+  //   const processPayment = async (e: React.FormEvent<HTMLFormElement>) => {
+  const processPayment = async () => {
+    // e.preventDefault();
+    try {
+      const orderId: string = await createOrderId();
+      const options = {
+        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+        amount: parseFloat(amount) * 100,
+        currency: currency,
+        name: "name",
+        description: "description",
+        order_id: orderId,
+        handler: async function (response: any) {
+          const data = {
+            orderCreationId: orderId,
+            razorpayPaymentId: response.razorpay_payment_id,
+            razorpayOrderId: response.razorpay_order_id,
+            razorpaySignature: response.razorpay_signature,
+          };
+
+          const result = await fetch("/api/verify", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+          });
+          const res = await result.json();
+          if (res.isOk) alert("payment succeed");
+          else {
+            alert(res.message);
+          }
+        },
+        prefill: {
+          name: name,
+          email: email,
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+      const paymentObject = new window.Razorpay(options);
+      paymentObject.on("payment.failed", function (response: any) {
+        alert(response.error.description);
+      });
+      paymentObject.open();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  processPayment();
+
+  return (
+    <Script
+      id="razorpay-checkout-js"
+      src="https://checkout.razorpay.com/v1/checkout.js"
+    />
+  );
+}
+
+export default Payment;
