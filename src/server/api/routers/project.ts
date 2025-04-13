640fc64e576f0143dc0d8d7a1cd4d94f7f5502bb
Pratush Bose <bosepratush@gmail.com>
2025-04-13 18:20:54 +0530

    adding getusercredit method and the accompanying logic to deduct user credits while creating project

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,9 +1,10 @@
 import { pollCommits } from "~/lib/github";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
-import { indexGithubRepo } from "~/lib/github-loader";
+import { checkCredits, indexGithubRepo } from "~/lib/github-loader";
 import { askQuestion } from "~/lib/langchain-utils";
 import MeetingCard from "~/app/(protected)/dashboard/meeting-card";
+import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export const projectRouter = createTRPCRouter({
   createProject: protectedProcedure
@@ -15,7 +16,19 @@ export const projectRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      console.log("userID: ", ctx.user.userId);
+      const user = await ctx.db.user.findUnique({
+        where: { id: ctx.user.userId! },
+        select: { credits: true },
+      });
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      const currentCredits = user.credits || 0;
+      const fileCount = await checkCredits(input.githubUrl, input.githubToken);
+      if (fileCount > currentCredits) {
+        throw new Error("Not enough credits");
+      }
       const project = await ctx.db.project.create({
         data: {
           name: input.name,
@@ -29,6 +42,10 @@ export const projectRouter = createTRPCRouter({
       });
       await indexGithubRepo(project.id, input.githubUrl, input.githubToken);
       await pollCommits(project.id);
+      await ctx.db.user.update({
+        where: { id: ctx.user.userId! },
+        data: { credits: { decrement: fileCount } },
+      });
       return project;
     }),
   getProjects: protectedProcedure.query(async ({ ctx }) => {
@@ -178,4 +195,16 @@ export const projectRouter = createTRPCRouter({
       // select: { credits: true },
     });
   }),
+  checkCredits: protectedProcedure
+    .input(
+      z.object({ githubUrl: z.string(), githubToken: z.string().optional() }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const fileCount = await checkCredits(input.githubUrl, input.githubToken);
+      const userCredits = await ctx.db.user.findUnique({
+        where: { id: ctx.user.userId! },
+        select: { credits: true },
+      });
+      return { fileCount, userCredits: userCredits?.credits || 0 };
+    }),
 });
