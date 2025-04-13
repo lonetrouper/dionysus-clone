bec572c30ae1b065adc6bd0b8fc4a06f7a48bc77
Pratush Bose <bosepratush@gmail.com>
2025-04-13 18:21:11 +0530

    added method to get file count

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,10 +1,62 @@
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
-import { PrismaVectorStore } from "@langchain/community/vectorstores/prisma";
 import { addSourceCodeEmbeddings, getCodeSummary } from "./langchain-utils";
-import { Prisma, SourceCodeEmbeddings } from "@prisma/client";
 import { db } from "~/server/db";
-import { VertexAIEmbeddings } from "@langchain/google-vertexai";
 import { Document } from "@langchain/core/documents";
+import { Octokit } from "octokit";
+
+const getFileCount = async (
+  path: string,
+  octokit: Octokit,
+  githubOwner: string,
+  githubRepo: string,
+  acc: number = 0,
+) => {
+  const { data } = await octokit.rest.repos.getContent({
+    owner: githubOwner,
+    repo: githubRepo,
+    path,
+  });
+
+  if (!Array.isArray(data) && data.type === "file") {
+    return acc + 1;
+  }
+
+  if (Array.isArray(data)) {
+    let fileCount = 0;
+    const directories: string[] = [];
+
+    for (const item of data) {
+      if (item.type === "file") {
+        fileCount++;
+      } else if (item.type === "dir") {
+        directories.push(item.path);
+      }
+    }
+
+    if (directories.length > 0) {
+      const directoryCounts = await Promise.all(
+        directories.map((dirPath) =>
+          getFileCount(dirPath, octokit, githubOwner, githubRepo, 0),
+        ),
+      );
+      fileCount += directoryCounts.reduce((acc, dirCount) => acc + dirCount, 0);
+    }
+
+    return fileCount; // Add this return statement
+  }
+
+  return acc; // Default return
+};
+export const checkCredits = async (githubUrl: string, githubToken?: string) => {
+  const octokit = new Octokit({ auth: githubToken });
+  const githubOwner = githubUrl.split("/")[3];
+  const githubRepo = githubUrl.split("/")[4];
+  if (!githubOwner || !githubRepo) {
+    return 0;
+  }
+  const fileCount = await getFileCount("", octokit, githubOwner, githubRepo, 0);
+  return fileCount;
+};
 
 const githubLoader = async (githubUrl: string, githubToken?: string) => {
   const loader = new GithubRepoLoader(githubUrl, {
